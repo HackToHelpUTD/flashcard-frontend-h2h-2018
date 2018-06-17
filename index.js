e0d65c5e9387726ce77368c59c5bb6718206a289
Ata Gowani <atagowani@gmail.com>
2018-06-17 16:42:59 -0500

    Add `/api/alltopics` endpoint

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var https = require('https');
 
 app.use('/src', express.static(path.join(__dirname, 'src')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.get('/api/alltopics', (req, res) => {
+  https.get('https://fcg-api.herokuapp.com/api/alltopics', (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+    // The whole response has been received. Print out the result.
+    response.on('end', () => {
+      console.log(JSON.parse(data));
+      res.json(JSON.parse(data));
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+});
+
+app.get('/api/topics', (req, res) => {
+  var topic_arg = req.query.topic;
+
+  https.get(`https://fcg-api.herokuapp.com/api/alltopics/api/topics/topic?=${topic_arg}`, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+    // The whole response has been received. Print out the result.
+    response.on('end', () => {
+      console.log(JSON.parse(data));
+      res.json(JSON.parse(data));
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
