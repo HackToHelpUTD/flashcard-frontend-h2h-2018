78cbc2bde5ed5640458af515fe7b76035f2c48dd
sverma2019 <sverma2019@gmail.com>
2018-06-23 15:58:21 -0500

    Add Subtopics Page and Back Button

--- src/js/subtopics.js
+++ src/js/subtopics.js
@@ -5,8 +5,6 @@ function loadSubTopics(topic) {
   subTopicsAPI(topic);
 }
 
-
-
 function createSubTopicsPage(topic) {
   //Title
   title = document.createElement("div"); 
@@ -15,30 +13,29 @@ function createSubTopicsPage(topic) {
   title.classList.add("title");
   var titles = document.getElementsByClassName("title")[0];
   titles.appendChild(title);
+  //Calls the back button function
+  backToTopics();
 }
 
-
-
+//Obtains subtopics APIs
 function subTopicsAPI(topic) {
-  //APIs
   var subtopics;
-
-    var request = new XMLHttpRequest(); //create a new instance of XMLHttpRequest object
-    request.onreadystatechange =  function() {
-    if(request.readyState == 4 && request.status == 200) {
+  var request = new XMLHttpRequest(); //create a new instance of XMLHttpRequest object
+  request.onreadystatechange =  function() {
+  if(request.readyState == 4 && request.status == 200) {
       console.log(request.response);
       var obj = JSON.parse(request.response);
       subtopics = obj.subtopics;
       console.log(subtopics);
       subTopicButtons(subtopics);
     }
-  
   }
   request.open("GET", "/api/topics?topic=" + topic);
   request.send();
 }
 
 function subTopicButtons(subtopics) {
+
    for(var i = 0; i < subtopics.length; i++) {
     var button = document.createElement("button");
     var box = document.getElementsByClassName("box")[0];
@@ -47,4 +44,24 @@ function subTopicButtons(subtopics) {
     button.classList.add("button-style", "btn", "btn-light", "btn-block");
     box.appendChild(button);
   }
+
 }
+
+//Back Button
+function backToTopics()
+{
+  var backButton = document.createElement("img");
+  backButton.src="src/imgs/back-button.png";
+  backButton.classList.add("back-Button");
+
+  backButton.addEventListener("click", function() {
+    var container = document.getElementsByClassName("container")[0];
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    init();
+  });
+  var placeImg = document.getElementById("backButton");
+  placeImg.appendChild(backButton);
+}
+
