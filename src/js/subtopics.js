c5c7a41d4bca8a19f123aac4febb11c06fafba6e
Jonathan-Harty <jonathanharty99@gmail.com>
2018-07-08 13:37:32 -0500

    Fix start bug

--- src/js/subtopics.js
+++ src/js/subtopics.js
@@ -153,7 +153,15 @@ function createModal() {
   start.innerHTML = "Start!";
 
   start.addEventListener("click", function() {
-    initFlashcardPage(session_info.topic, session_info.subtopic, session_info.type, session_info.difficulty);
+    if(session_info.type != "" && session_info.difficulty != "")
+    {
+      initFlashcardPage(session_info.topic, session_info.subtopic, session_info.type, session_info.difficulty);
+    }
+    else
+    {
+      alert("Not All Attributes Selected");
+    }
+    
   });
 
   // Appending to the Modal Body 
